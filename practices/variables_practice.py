3f9b5e8cf9752f52ab7a6154056bbb80e7a20cc0
soappp12 <sophia.baggaley@ucas-edu.net>
2025-09-02 13:30:20 -0600

    variable practice

---
+++ practices/variables_practice.py
@@ -0,0 +1,13 @@
+# SB 6th Variables Practice
+
+name = "Sophia"
+number1 = "5" 
+number2 = "600"
+breakfast = "bagel"
+color = "blue"
+schoolname = "UCAS"
+year = "2025"
+eyecolor = "brown"
+age = "14" 
+favoritesubject = "none"
+print(name, number1, number2, breakfast, schoolname, year, eyecolor, age, favoritesubject)
