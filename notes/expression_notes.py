daf4ce3559572ec47807b06cbed2a65f93e66e00
soappp12 <sophia.baggaley@ucas-edu.net>
2025-09-04 13:43:33 -0600

    notes expressions

---
+++ notes/expression_notes.py
@@ -0,0 +1,48 @@
+# SB 6th Expression notes
+
+# What is an algorithm?
+ # a set of instructions to acomplish a task
+name = input("What is your name? ")
+print("Hello", name)
+
+#algorithm for area
+length = 5 
+width = 20
+area = length * width 
+print(length, "X", width, "=", area)
+
+# List steps in an algorithm
+ # 1. determine what information you need
+ # 2. complete any processes needed (process info)
+ # 3. provide an output 
+
+#1
+age_one = 14
+age_two = 14
+age_three = 14
+age_four = 15 
+#2
+total = age_one + age_two + age_three + age_four 
+average = total/4 
+#3
+print("the average age between: ", age_one, ",", age_two, ",", age_three, ",", age_four, "=", average)
+
+#Math equations!
+num_one = 12
+num_two = 4
+
+print("addition:", num_one + num_two)
+print("subtraction:", num_one - num_two)
+print("multiplication:", num_one * num_two)
+print("division:", num_one / num_two)
+print("exponents:", num_one ** num_two)
+print("integer division:", num_one // num_two)
+print("modulo (remainder):", num_one % num_two)
+
+# List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+ # + -> add
+ # - -> subtract
+ # * -> multiply
+ # / -> divide
+#List ALL of the different assignment operators (Give me the symbols and what it does)
+ #
