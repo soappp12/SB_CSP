a0d6172b7dc0d24ca4c271e0c6c0e9be1cf64bc7
soappp12 <sophia.baggaley@ucas-edu.net>
2025-09-08 13:44:20 -0600

    start notes

--- notes/string.py
+++ notes/string.py
@@ -3,15 +3,26 @@
 print("I did it!")
 
 # 1. What makes something a string?
+ # a collection of symbols that are held together by quotation marks. Every user input is a string
+ # examples of strings
+first_name = input("What is your first name?:\n").strip().title()
+
+last_name = input("What is your last name:\n").strip().title()
+
+full_name = first_name + " " + last_name
+
+sentence = "The quick brown fox jumps over the lazy dog." 
+
+print("Welome to my program", full_name + "!")
 
 # 2. Why do we have strings?
-
+ # they are the only data type that allows us to use letters
 # 3. How do stupid proofing and sanitization relate to each other?
-
+ #
 # 4. What is debugging?
-
+ #
 # 5. How do you debug the different types of errors?
-
+ #
 # 6. Describe what each of the methods below does:
  # find()
 
