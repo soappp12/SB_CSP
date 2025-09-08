eadf51ed442e45aab41756ebbe0367a51049055c
soappp12 <sophia.baggaley@ucas-edu.net>
2025-09-08 13:06:02 -0600

    noytes

---
+++ notes/string.py
@@ -0,0 +1,24 @@
+# SB 6th String notes
+
+print("I did it!")
+
+# 1. What makes something a string?
+
+# 2. Why do we have strings?
+
+# 3. How do stupid proofing and sanitization relate to each other?
+
+# 4. What is debugging?
+
+# 5. How do you debug the different types of errors?
+
+# 6. Describe what each of the methods below does:
+ # find()
+
+ # concatenate (add)
+
+ # upper()
+
+ # lower()
+
+ # strip()
