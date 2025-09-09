779c6fa486fc1cc2f7f58db87ffe6dccaca23c87
soappp12 <sophia.baggaley@ucas-edu.net>
2025-09-09 13:43:36 -0600

    string notes

--- notes/string.py
+++ notes/string.py
@@ -17,23 +17,31 @@ print("Welome to my program", full_name + "!")
 
 # 2. Why do we have strings?
  # they are the only data type that allows us to use letters
+
 # 3. How do stupid proofing and sanitization relate to each other?
- #
+ # 
 # 4. What is debugging?
+ # a bug is any erro in your code that keeps it from running or making it run inproperly
  # debugging is fixing problems in my code
- # syntax error
- # logicerror
- # run-time error
- #  
+ # syntax error: writing a variable wrong, idents where they arent supposed to be
+string = 'this is my string'
+ # logic error: code does something that we didnt expect it to do
+num0ne = "1"
+numTwo = "3"
+print(num0ne + numTwo)
+ # run-time error: when there is a problem in the code that would break the code when it trys to run
+
 # 5. How do you debug the different types of errors?
- #
+ # you fix the error yourself
 # 6. Describe what each of the methods below does:
- # find()
+ # find() retuns the index number of that paticular item
+print(sentence.find("brown"))
+print(sentence[10:15]) # slice
 
- # concatenate (add)
+ # concatenate (add) you add strings together
 
- # upper()
+ # upper() makes the word uppercase
 
- # lower()
+ # lower() makes the word lowercase 
 
- # strip()
+ # strip() gets rid of all the extra spacing before and after a code
