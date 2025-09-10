4e11d3fefebc64cb07f331c3ef85648e0fcc7c17
soappp12 <sophia.baggaley@ucas-edu.net>
2025-09-10 13:41:40 -0600

    finish silly setnences

---
+++ practices/silly_sentences.py
@@ -0,0 +1,8 @@
+# SB 6th Silly sentences
+
+word_one = input("Write a noun:\n")
+word_two = input("Write a noun:\n")
+word_three = input("Write an ajective:\n")
+word_four = input("Write a noun:\n")
+word_five = input("Write a noun:\n")
+print("If I could go anywhere in the world I would go to", word_one, ",", "for a vacation, I would go to", word_two, ".", "It is", word_three, "there.", "The", word_four, "are enormous and the", word_five, "is sweet")
