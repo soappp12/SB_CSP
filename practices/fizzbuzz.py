61941a2c6f907da26c02af2fb40e73527fb83d9a
soappp12 <sophia.baggaley@ucas-edu.net>
2025-09-23 13:40:35 -0600

    fizzbuzz assignment

---
+++ practices/fizzbuzz.py
@@ -0,0 +1,14 @@
+# SB 6th Fizzbuzz
+
+i = 1
+
+while i < 21:
+    if i % 5 == 0 and i % 3 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(f"{i}")
+    i += 1
