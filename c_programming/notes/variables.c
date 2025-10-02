042630b7265f5426732bdc36f843b63973abef43
soappp12 <sophia.baggaley@ucas-edu.net>
2025-10-02 13:44:06 -0600

    notes

---
+++ c_programming/notes/variables.c
@@ -0,0 +1,44 @@
+// SB 6th Variables Notes
+#include <stdio.h>
+
+int main(void){
+    int num = 4;
+    float pi = 3.14;
+    char grade = 'A';// Will only hold 1 letter
+    char name[]= "LaRose";
+    //bool passing = true;
+    printf("%d", num);
+
+    return 0;
+}
+// What is the main difference between declaring variables in Python and C?
+ // in C you have to set what type of data the variable holds
+
+// In C, what is the purpose of specifying a data type when declaring a variable?
+ // 
+
+// List three common data types used in C and their corresponding format specifiers for printf().
+ //
+// How do you declare and initialize an integer variable named "age" with the value 25 in C?
+
+// What is the difference between printf() and scanf() functions in C?
+
+// Write a C program that asks the user for their name and age, then prints a greeting message with their information.
+
+// How do you add comments in C? What are the two types of comments?
+
+// What is the purpose of the #include <stdio.h> line at the beginning of a C program?
+
+// In C, what is the significance of the main() function?
+
+// Write a C program that calculates the area of a rectangle. Ask the user to input the length and width, then display the result.
+
+// What is the difference between %d and %f format specifiers in printf()?
+
+// How do you print a newline character in C?
+
+// Write a C program that converts temperature from Celsius to Fahrenheit. Ask the user for input in Celsius and display the result in Fahrenheit.
+
+// What is the purpose of the & symbol when using scanf() to get user input?
+
+// How do you declare a constant in C? Provide an example.
