af3439a8fd83bf01e236bf3a54b8c9ebf02a8961
soappp12 <sophia.baggaley@ucas-edu.net>
2025-10-07 05:09:00 +0000

    first program

---
+++ c_programming/notes/firstprogram.c
@@ -0,0 +1,11 @@
+// SB 6th First program C
+#include <stdio.h>
+
+int main(void){
+    char name[10];
+    printf("What is your name?:\n");
+    scanf("%s", name);
+    printf("Hi, %s", name);
+
+    return 0;
+}
