e28609ba141e94db0d4e0bc28a9c13254474a972
soappp12 <sophia.baggaley@ucas-edu.net>
2025-10-07 05:25:52 +0000

    variable practice

---
+++ c_programming/notes/variablepractice.c
@@ -0,0 +1,25 @@
+// SB 6th Variable Practice
+#include <stdio.h>
+int main(void){
+    char name[25] = "Sophia";
+    int number1to10 = 9;
+    float gpa = 3.3;
+    char breakfast[25] = "Bagel";
+    char favcolor[25] = "Blue";
+    char schoolname[25] = "Ucas";
+    int year = 2025;
+    char eyecolor[25] = "Brown";
+    char favsubject [25]= "History";
+    int age = 14;
+    printf("Name: %s\n", name);
+    printf("Number1to10: %d\n", number1to10);
+    printf("gpa: %.1f\n", gpa);
+    printf("breakfast: %s\n", breakfast);
+    printf("favcolor: %s\n", favcolor);
+    printf("school name: %s\n", schoolname);
+    printf("year: %d\n", year);
+    printf("eye color: %s\n", eyecolor);
+    printf("age: %d\n", age);
+    printf("fav subject: %s\n", favsubject);
+    return 0;
+}
