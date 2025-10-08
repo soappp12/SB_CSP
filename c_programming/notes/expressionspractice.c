695910f7cb6254c2d95aa3ce2581f9a5b06eacf7
soappp12 <sophia.baggaley@ucas-edu.net>
2025-10-08 13:43:26 -0600

    expression practice

---
+++ c_programming/notes/expressionspractice.c
@@ -0,0 +1,18 @@
+// SB 6th expression practice
+#include <stdio.h>
+#include <math.h>
+int main(void){
+
+    printf("7-24/8*4+6 = %d\n", (int)7-24/8*4+6);
+    printf("18/3-7+2*5= %d\n", (int)18/3-7+2*5);
+    printf("6*4/12+72/8-9= %d\n", (int)6*4/12+72/8-9);
+    printf("(17-6/2)+4*3= %d\n", (int)(17-6/2)+4*3);
+    printf("-2*(1*4-2/2)+(6+2-3)= %d\n", (int)-2*(1*4-2/2)+(6+2-3));
+    printf("-1*((3-4*7)/5)-2*24/6= %d\n", (int)(3-4*7)/5*-1-2*24/6);
+    printf("(3*5^2/15)-(5-2^2)= %d\n",(int)(3*(int)pow(5, 2)/15)-(5-(int)pow(2, 2)));
+    printf("(1^4*2^2+3^3)-2^5/4= %d\n",(int)((int)pow(1, 4)*(int)pow(2, 2)+(int)pow(3, 3))-(int)pow(-2, 5)/4);
+    return 0;
+
+}
+
+
--- c_programming/notes/tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-int main(void) {
-    float celsius;
-    float farhenheit;
-    printf("Whats the temperature in celsius? \n");
-    scanf("%f", &celsius);
-    farhenheit = celsius * 1.8 + 32;
-    printf("The fahrenheit temperature is, %f", farhenheit);
-    return 0;
-}
