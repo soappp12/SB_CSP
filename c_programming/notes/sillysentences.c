c13e47458c980564958b366c9b7e7a47a6737b10
soappp12 <sophia.baggaley@ucas-edu.net>
2025-10-10 13:43:32 -0600

    start silly sentences

---
+++ c_programming/notes/sillysentences.c
@@ -0,0 +1,15 @@
+// SB 6th Silly sentences
+#include <stdio.h>
+#include <string.h>
+
+char word_1[50];
+char word_2[50];
+char word_3[50];
+char word_4[50];
+char word_5[50];
+
+printf("Write a noun \n");
+scanf("%s\n", word_1);
+
+printf("Write a noun: \n");
+scanf("%s\n", word_2);
