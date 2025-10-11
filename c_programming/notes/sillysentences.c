e8b50fcbe56926e377241d7e4be926b550507168
soappp12 <sophia.baggaley@ucas-edu.net>
2025-10-11 04:37:31 +0000

    finish silly sentences

--- c_programming/notes/sillysentences.c
+++ c_programming/notes/sillysentences.c
@@ -2,14 +2,30 @@
 #include <stdio.h>
 #include <string.h>
 
-char word_1[50];
-char word_2[50];
-char word_3[50];
-char word_4[50];
-char word_5[50];
+int main(void){
+    char word_1[50];
+    char word_2[50];
+    char word_3[50];
+    char word_4[50];
+    char word_5[50];
 
-printf("Write a noun \n");
-scanf("%s\n", word_1);
+    printf("Write a noun \n");
+    scanf("%s", word_1);
 
-printf("Write a noun: \n");
-scanf("%s\n", word_2);
+    printf("Write a noun: \n");
+    scanf("%s", word_2);
+
+    printf("Write an ajective: \n");
+    scanf("%s", word_3);
+
+    printf("Write a noun: \n");
+    scanf("%s", word_4);
+
+    printf("Write a noun: \n");
+    scanf("%s", word_5);
+
+    printf("If I could go anywhere in the world I would go to %s, for a vacation, I would go to %s. It is %s there. The %s are enormous and the %s is sweet.\n", word_1, word_2, word_3, word_4, word_5);
+
+    return 0;
+}
+
